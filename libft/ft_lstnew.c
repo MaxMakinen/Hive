7a7c7ae25ec5519353507ccfa01d803d7825fb83
Max Mäkinen <quantumhound@gmail.com>
2021-11-25 18:57:58 +0200

    changed lstnew

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mmakinen <mmakinen@hive.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 10:02:43 by mmakinen          #+#    #+#             */
-/*   Updated: 2021/11/25 18:47:05 by mmakinen         ###   ########.fr       */
+/*   Updated: 2021/11/25 18:56:45 by mmakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*newnode;
-	char	*newcont;
+	void	*newcont;
 
 	newnode = malloc(sizeof(t_list));
 	if (newnode == 0)
@@ -27,7 +27,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	}
 	else
 	{
-		newcont = malloc(sizeof(*content) * (content_size));
+		newcont = malloc(sizeof(*newcont) * content_size);
 		if (newcont == 0)
 		{
 			free(newnode);
