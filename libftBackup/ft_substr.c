b2c96db838d05cd281aed6322eeb87d0755ad162
Max Makinen <mmakinen@c1r1p4.hive.fi>
2021-12-07 16:17:12 +0200

    moved libft to libftBackup

---
+++ libftBackup/ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakinen <mmakinen@hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 14:54:09 by mmakinen          #+#    #+#             */
+/*   Updated: 2021/11/29 12:41:37 by mmakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*newstr;
+	size_t	counter;
+
+	counter = 0;
+	if (s == 0)
+		return (NULL);
+	newstr = ft_strnew(len);
+	if (newstr == 0)
+		return (NULL);
+	if (start < ft_strlen(s))
+	{
+		while (counter < len)
+		{
+			newstr[counter] = s[start + counter];
+			counter++;
+		}
+	}
+	newstr[counter] = '\0';
+	return (newstr);
+}
