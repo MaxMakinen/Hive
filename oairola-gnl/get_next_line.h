eba6f04e682ee46cae50124734cc69218da1ab23
Max Mäkinen <quantumhound@gmail.com>
2022-01-05 14:05:59 +0200

    eval

---
+++ oairola-gnl/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oairola <oairola@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 10:43:37 by oairola           #+#    #+#             */
+/*   Updated: 2021/12/14 15:00:17 by oairola          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+# define FD_MAX 8192
+
+int	get_next_line(const int fd, char **line);
+
+#endif
