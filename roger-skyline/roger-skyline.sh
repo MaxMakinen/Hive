e2dd3714344eb8a6db5f4a7d44f8e21af5081524
mmakinen <quantumhound@gmail.com>
2022-06-28 11:46:43 +0300

    starting on roger-skyline

---
+++ roger-skyline/roger-skyline.sh
@@ -0,0 +1,6 @@
+VBoxManage createvm --name "roger-skyline-1" --register
+# Virtual machine 'roger-skyline-1' is created and registered.
+# UUID: a64228ad-94fb-4b32-a56b-5a31a8d73784
+# Settings file: '/Users/mmakinen/VirtualBox VMs/roger-skyline-1/roger-skyline-1.vbox'
+vboxmanage modifyvm "roger-skyline-1" --ostype debian_64
+
