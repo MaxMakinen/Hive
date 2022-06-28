bd6384e2ca285af2ac479b1686bb5a80b9d5710b
mmakinen <quantumhound@gmail.com>
2022-06-28 12:04:09 +0300

    shebvang

--- roger-skyline/roger-skyline.sh
+++ roger-skyline/roger-skyline.sh
@@ -1,6 +1,19 @@
-VBoxManage createvm --name "roger-skyline-1" --register
-# Virtual machine 'roger-skyline-1' is created and registered.
-# UUID: a64228ad-94fb-4b32-a56b-5a31a8d73784
-# Settings file: '/Users/mmakinen/VirtualBox VMs/roger-skyline-1/roger-skyline-1.vbox'
-vboxmanage modifyvm "roger-skyline-1" --ostype debian_64
+#!/bin/bash
+# https://andreafortuna.org/2019/10/24/how-to-create-a-virtualbox-vm-from-command-line/
 
+if [ $# == 1 ]; then
+	MACHINENAME=$1
+else
+	MACHINENAME="roger-skyline-1"
+fi
+
+VBoxManage createvm --name $MACHINENAME --ostype "Debian_64" --register --basefolder 'pwd'
+
+# Enable I/O APIC: Advanced Programmable Interrupt Controllers (APICs) are an x86 hardware feature that have replaced Programmable Interrupt Controllers (PICs). 
+# With an I/O APIC, OSes can use more than 16 interrupt requests (IRQs) and therefore avoid IRQ sharing for improved reliability.
+# Note: Enabling the I/O APIC is required, especially for 64-bit Windows guest OSes. It is also required if you want to use more than one virtual CPU in a virtual machine.
+VBoxManage modifyvm $MACHINENAME --ioapic on
+# --memory <memorysize>: Sets the amount of RAM, in MB, that the virtual machine should allocate for itself from the host.
+# --vram <vramsize>: Sets the amount of RAM that the virtual graphics card should have.
+VBoxManage modifyvm $MACHINENAME --memory 1024 --vram 128
+VBoxManage modifyvm $MACHINENAME --nic1 nat
